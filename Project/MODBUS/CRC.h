f3bb2f545bb309e99b28454f2ecc40d37bbd2f7a
andongzhang <1915569230@qq.com>
2022-07-12 17:50:11 +0800

    添加MODBUS程序

---
+++ Project/MODBUS/CRC.h
@@ -0,0 +1,9 @@
+#ifndef __CRC_H
+#define __CRC_H
+
+#include "hidef.h"
+#include "derivative.h"
+
+extern unsigned int Crc16_MODBUS (unsigned int Crc16);
+
+#endif
---
+++ Project/MODBUS/MODBUS.h
@@ -0,0 +1,9 @@
+#ifndef __MODBUS_H
+#define __MODBUS_H
+
+#include "hidef.h"
+#include "derivative.h"
+
+
+
+#endif
